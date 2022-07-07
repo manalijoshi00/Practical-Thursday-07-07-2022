47fde373fda8fd6f8f044a3b8ee730d1c4f1091a
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-07 16:07:16 +0530

    Add files via upload

---
+++ Caste.java
@@ -0,0 +1,18 @@
+class Caste
+{
+   public static void main(String[] args)
+   {
+     int i = 81;
+     char b = (char)i;
+     double d = 323.142f;
+     int c = (int)d;
+     float f = 72.38f;
+     int e = (int)f;
+     double d1 = 323.142f;
+     byte l = (byte)d1;
+     System.out.println("i =" + i + " and b = " + b);
+     System.out.println("d =" + d + " and c = " + c);
+     System.out.println("f =" + f + " and e = " + e);
+     System.out.println("d1 =" + d1 + " and l = " + l);
+    }
+}
