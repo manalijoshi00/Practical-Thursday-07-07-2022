6039c640625d09e87e6ac186519e762e1cc71de9
manalijoshi00 <108450658+manalijoshi00@users.noreply.github.com>
2022-07-07 16:03:35 +0530

    Add files via upload

---
+++ Notebook.java
@@ -0,0 +1,21 @@
+class Notebook 
+{ 
+    public static void main(String[] args) 
+    {
+       String brand = "idol";
+       double processorSpeedInGHz = 2.66; 
+       double ramSizeInGB = 8;            
+       int harddiskSizeInGB = 500;        
+       int monitorInInch = 15 ;           
+       double price = 1760.55;
+       System.out.println("notebook brand :" +brand); 
+       System.out.println("notebook processorSpeedInGHz :" +processorSpeedInGHz);
+       System.out.println("notebook ramSizeInGB :" +ramSizeInGB);
+       System.out.println("notebook harddiskSizeInGB :" +harddiskSizeInGB);
+       System.out.println("notebook monitorInInch :" +monitorInInch);
+       System.out.println("notebook price :" +price); 
+    }
+}
+ 
+ 
+      
